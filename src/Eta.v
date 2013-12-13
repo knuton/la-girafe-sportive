32c6cb521cf103bd0ca5816292f969d86a964dfe
Ignas Vyšniauskas <baliulia@gmail.com>
2013-12-13 01:55:29 +0100

    Add proof that eta is substitutive

--- src/Eta.v
+++ src/Eta.v
@@ -35,4 +35,42 @@ Definition eta_star_step := rt_step lterm eta.
 Definition eta_star_refl := rt_refl lterm eta.
 Definition eta_star_trans := rt_trans lterm eta.
 
+
+Lemma eta_prim_substitutive:
+  forall (M N L: lterm) (n: nat),
+  eta_prim M N -> eta_prim (subst n L M) (subst n L N).
+Proof.
+  intros.
+  assert (What: n + 1 = S n). omega.
+  destruct H.
+  assert (Hmm: subst n L (Lam (App f (Var 0))) =
+               Lam (App (subst (S n) L f) (Var 0))).
+  simpl.
+  rewrite What. auto.
+  rewrite Hmm.
+  apply eta_base.
+  unfold shift.
+  rewrite lift_lem2. simpl. rewrite H. rewrite What.
+  unfold shift.
+  reflexivity. omega.
+Qed.
+
+Lemma eta_substitutive:
+  forall (M N L: lterm) (n: nat),
+  eta M N -> eta (subst n L M) (subst n L N).
+Proof.
+  intros. generalize dependent n. generalize dependent L.
+  unfold eta.
+  induction H.
+  intros. apply clos_base. apply eta_prim_substitutive. assumption.
+  (* This seems to go ugly, but the intuition tactic works...
+     I wonder if this has to do anything with the `lt_dec` in lift.
+     Also, what is that {struct t} doing there?!
+     Also, am I doing this whole proof wrong?
+   *)
+  intros. apply clos_lam. intuition.
+  intros. apply clos_appl. intuition.
+  intros. apply clos_appr. intuition.
+Qed.
+
 End Eta.
