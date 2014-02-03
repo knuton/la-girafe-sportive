c763e46a22893ece46ceb94a179d9ed8ae680fcd
Johannes Emerich <johannes@emerich.de>
2014-02-03 23:46:30 +0100

    Rename in alignment with Kashima

--- src/Kashima.v
+++ src/Kashima.v
@@ -10,19 +10,19 @@ Require Import Coq.Logic.Classical_Prop.
 (* Predicate characterising abstractions. *)
 
 Inductive abstr : lterm -> Prop :=
-  | islam : forall t, abstr (Lam t).
+  | islam : forall M, abstr (Lam M).
 
 Example notabstr : ~ abstr (`"x").
 Proof. unfold not. intro. inversion H. Qed.
 
 (* Count the total number of redexes in a term. *)
 
-Fixpoint redcount (t : lterm) : nat :=
-  match t with
+Fixpoint redcount (M : lterm) : nat :=
+  match M with
   | Var i as v => 0
-  | App (Lam tinner) targ => (redcount tinner) + (redcount targ) + 1
-  | App t1 t2 => (redcount t1) + (redcount t2)
-  | Lam t => redcount t
+  | App (Lam N) N' => (redcount N) + (redcount N') + 1
+  | App N N' => (redcount N) + (redcount N')
+  | Lam N => redcount N
   end.
 
 Example countright :
@@ -38,18 +38,18 @@ Qed.
 *)
 
 Inductive nthred : nat -> lterm -> lterm -> Prop :=
-  | nthred_prim : forall t1 t2,
-      nthred 1 (App (Lam t1) t2) (subst 0 t2 t1)
-  | nthred_concr : forall n t1 t2 t3,
-      ~ abstr t1 -> nthred n t1 t2 -> nthred n (App t1 t3) (App t2 t3)
-  | nthred_concrplus : forall n t1 t2 t3,
-      abstr t1 -> nthred n t1 t2 -> nthred (n+1) (App t1 t3) (App t2 t3)
-  | nthred_concl : forall n t1 t2 t3,
-      ~ abstr t3 -> nthred n t1 t2 -> nthred (n + redcount t3) (App t3 t1) (App t3 t2)
-  | nthred_conclplus : forall n t1 t2 t3,
-      abstr t3 -> nthred n t1 t2 -> nthred (n + (redcount t3) + 1) (App t3 t1) (App t3 t2)
-  | nthred_abst : forall n t1 t2,
-      nthred n t1 t2 -> nthred n (Lam t1) (Lam t2).
+  | nthred_prim : forall A B,
+      nthred 1 (App (Lam A) B) (subst 0 B A)
+  | nthred_concr : forall n A B C,
+      ~ abstr A -> nthred n A B -> nthred n (App A C) (App B C)
+  | nthred_concrplus : forall n A B C,
+      abstr A -> nthred n A B -> nthred (n+1) (App A C) (App B C)
+  | nthred_concl : forall n A B C,
+      ~ abstr C -> nthred n A B -> nthred (n + redcount C) (App C A) (App C B)
+  | nthred_conclplus : forall n A B C,
+      abstr C -> nthred n A B -> nthred (n + (redcount C) + 1) (App C A) (App C B)
+  | nthred_abst : forall n A B,
+      nthred n A B -> nthred n (Lam A) (Lam B).
 
 Example prim : nthred 1 ((\"x" ~> `"x") $ `"y") (`"y").
 Proof. apply nthred_prim. Qed.
@@ -94,7 +94,7 @@ Qed.
    Equivalence with the definition found in [Beta] has yet to be proven (if necessary).
 *)
 
-Definition bet (t1 t2 : lterm) : Prop := exists n : nat, nthred n t1 t2.
+Definition bet (A B : lterm) : Prop := exists n : nat, nthred n A B.
 
 Definition betstar := clos_refl_trans lterm bet.
 Definition betstar_step := rt_step lterm bet.
@@ -103,7 +103,7 @@ Definition betstar_trans := rt_trans lterm bet.
 
 (* Left-most reduction is simply reduction of the first redex. *)
 
-Definition lmost (t1 t2 : lterm) : Prop := nthred 1 t1 t2.
+Definition lmost (A B : lterm) : Prop := nthred 1 A B.
 
 Definition lstar := clos_refl_trans lterm lmost.
 Definition lstar_step := rt_step lterm lmost.
@@ -113,29 +113,29 @@ Definition lstar_trans := rt_trans lterm lmost.
 (** Head-reduction in application (Definition 3.1) **)
 
 Inductive hap' : lterm -> lterm -> Prop :=
-  | hap'_hred : forall t0 t1, hap' (App (Lam t0) t1) (subst 0 t1 t0)
-  | hap'_hreds : forall t0 t1 ta, hap' t0 t1 -> hap' (App t0 ta) (App t1 ta).
+  | hap'_hred : forall A B, hap' (App (Lam A) B) (subst 0 B A)
+  | hap'_hreds : forall A B C, hap' A B -> hap' (App A C) (App B C).
 
 Inductive hap : lterm -> lterm -> Prop :=
-  | hap_refl : forall t, hap t t
-  | hap_hred : forall t0 t1, hap' t0 t1 -> hap t0 t1
-  | hap_trans : forall t1 t2 t3, hap t1 t2 -> hap t2 t3 -> hap t1 t3.
+  | hap_refl : forall A, hap A A
+  | hap_hred : forall A B, hap' A B -> hap A B
+  | hap_trans : forall A B C, hap A B -> hap B C -> hap A C.
 
 (** Standard reduction (Definition 3.2) **)
 
 Inductive st : lterm -> lterm -> Prop :=
-  | st_hap : forall t i, hap t (Var i) -> st t (Var i)
-  | st_hap_st_st : forall t t1 t2 t1' t2',
-      hap t (App t1 t2) -> st t1 t1' -> st t2 t2' -> st t (App t1' t2')
-  | st_haplam_st : forall t t1 t1',
-      hap t (Lam t1) -> st t1 t1' -> st t (Lam t1').
+  | st_hap : forall L x, hap L (Var x) -> st L (Var x)
+  | st_hap_st_st : forall L A B C D,
+      hap L (App A B) -> st A C -> st B D -> st L (App C D)
+  | st_haplam_st : forall L A B,
+      hap L (Lam A) -> st A B -> st L (Lam B).
 
 (** Lemma 3.3 **)
 
 (* (1) *)
-Lemma hap_lstar : forall t1 t2, hap t1 t2 -> lstar t1 t2.
+Lemma hap_lstar : forall M N, hap M N -> lstar M N.
 Proof.
-  intros. induction H as [ t | t0 t1 | t1 t2 t3]. (*case H.*)
+  intros. induction H as [ t | t0 t1 | t1 t2 t3].
     (* hap_refl *)
     apply lstar_refl.
     (* hap_hred *)
@@ -151,36 +151,36 @@ Qed.
 (** Lemma 3.4 **)
 
 (* (1) *)
-Lemma st_refl : forall t, st t t.
+Lemma st_refl : forall M, st M M.
 Proof.
-  intros t.
-  induction t.
+  intros M.
+  induction M.
     apply st_hap. apply hap_refl.
-    apply st_haplam_st with (t1 := t). apply hap_refl. assumption.
-    apply st_hap_st_st with (t1 := t1) (t2 := t2). apply hap_refl. assumption. assumption.
+    apply st_haplam_st with (A := M). apply hap_refl. assumption.
+    apply st_hap_st_st with (A := M1) (B := M2). apply hap_refl. assumption. assumption.
 Qed.
 
 (* (2) *)
-Lemma hap__app_hap : forall t1 t2 t3,
-  hap t1 t2 -> hap (App t1 t3) (App t2 t3).
+Lemma hap__app_hap : forall M N P,
+  hap M N -> hap (App M P) (App N P).
 Proof.
   intros. induction H.
     apply hap_refl.
     apply hap_hred. apply hap'_hreds. assumption.
-    apply hap_trans with (t2 := App t2 t3); assumption.
+    apply hap_trans with (B := App B P); assumption.
 Qed.
 
 (* (3) *)
-Lemma hap_st__st : forall t1 t2 t3,
-  hap t1 t2 -> st t2 t3 -> st t1 t3.
+Lemma hap_st__st : forall L M N,
+  hap L M -> st M N -> st L N.
 Proof.
   intros. induction H0.
-    apply st_hap. apply hap_trans with (t2 := t). assumption. assumption.
-    apply st_hap_st_st with (t1 := t0) (t2 := t2).
-      apply hap_trans with (t2 := t); assumption.
+    apply st_hap. apply hap_trans with (B := L0). assumption. assumption.
+    apply st_hap_st_st with (A := A) (B := B).
+      apply hap_trans with (B := L0); assumption.
       assumption. assumption.
-    apply st_haplam_st with (t1 := t0).
-      apply hap_trans with (t2 := t); assumption.
+    apply st_haplam_st with (A := A).
+      apply hap_trans with (B := L0); assumption.
       assumption.
 Qed.
 
@@ -188,14 +188,14 @@ Qed.
 
 (* We first prove [subst_hap'], after which the actual lemma is easy to show. *)
 
-Lemma subst_hap' : forall t1 t2 t3, forall n,
-  hap' t1 t2 -> hap' (subst n t3 t1) (subst n t3 t2).
+Lemma subst_hap' : forall M N P, forall n,
+  hap' M N -> hap' (subst n P M) (subst n P N).
 Proof.
   intros. induction H.
     (* hap'_hred *)
     simpl.
-    assert (EXP: subst n t3 (subst 0 t1 t0) = subst 0 (subst n t3 t1) (subst (n+1) t3 t0)).
-      simpl. assert (T1: subst n t3 t1 = subst (n - 0) t3 t1).
+    assert (EXP: subst n P (subst 0 B A) = subst 0 (subst n P B) (subst (n+1) P A)).
+      simpl. assert (T1: subst n P B = subst (n - 0) P B).
         assert (T2: n = n - 0). omega. rewrite <- T2. reflexivity.
       rewrite T1. apply subst_lemma. omega.
     rewrite EXP. apply hap'_hred.
@@ -203,23 +203,23 @@ Proof.
     apply hap'_hreds. apply IHhap'.
 Qed.
 
-Lemma hap__hap_subst : forall t1 t2 t3,
-  hap t1 t2 -> forall n : nat, hap (subst n t3 t1) (subst n t3 t2).
+Lemma hap__hap_subst : forall M N P,
+  hap M N -> forall n : nat, hap (subst n P M) (subst n P N).
 Proof.
-  intros t1 t2 t3 H n. induction H.
+  intros M N P H n. induction H.
     apply hap_refl.
     apply hap_hred. apply subst_hap'. assumption.
-    apply hap_trans with (t2 := subst n t3 t2); assumption.
+    apply hap_trans with (B := subst n P B); assumption.
 Qed.
 
 (* We need to do quite a bit of busywork for (5). *)
 
 (* First, lift-independence of [hap], which is easy. *)
 
-Lemma lift_hap' : forall t t',
-  hap' t t' -> forall n k, hap' (lift n k t) (lift n k t').
+Lemma lift_hap' : forall M N,
+  hap' M N -> forall n k, hap' (lift n k M) (lift n k N).
 Proof.
-  intros t t' H. induction H.
+  intros M N H. induction H.
     (* hap'_hred *)
     intros n k. simpl.
     rewrite lift_distr_subst.
@@ -229,8 +229,8 @@ Proof.
     intros n k. rewrite lift_app. rewrite lift_app. apply hap'_hreds. apply (IHhap' n).
 Qed.
 
-Lemma lift_hap : forall t t',
-  hap t t' -> forall n k, hap (lift n k t) (lift n k t').
+Lemma lift_hap : forall M N,
+  hap M N -> forall n k, hap (lift n k M) (lift n k N).
 Proof.
   intros. induction H.
     (* hap_refl *)
@@ -238,17 +238,17 @@ Proof.
     (* hap_hreds *)
     apply hap_hred. apply lift_hap'. assumption.
     (* hap_trans *)
-    apply hap_trans with (t2 := lift n k t2); assumption.
+    apply hap_trans with (B := lift n k B); assumption.
 Qed.
 
 (* Second, lift-independence for [st], which is more intricate. *)
 
-Lemma hap'_no_lhs_var : forall i t,
-  ~ hap' (Var i) t.
+Lemma hap'_no_lhs_var : forall i M,
+  ~ hap' (Var i) M.
 Proof. intros. unfold not. intro H. inversion H. Qed.
 
-Lemma hap_lefthandside_var : forall i t,
-  hap (Var i) t -> t = Var i.
+Lemma hap_lefthandside_var : forall i M,
+  hap (Var i) M -> M = Var i.
 Proof.
   intros. dependent induction H.
     reflexivity.
@@ -265,24 +265,24 @@ Example lift_first_hap_ex2 :
     (Var 3).
 Proof.
   simpl.
-  apply hap_trans with (t2 := App (Lam (Var 4)) (Var 11)).
+  apply hap_trans with (B := App (Lam (Var 4)) (Var 11)).
   apply hap_hred.
     apply hap'_hreds. apply hap'_hred.
     assert (Var 3 = subst 0 (Var 11) (Var 4)). reflexivity. rewrite H.
     apply hap_hred. apply hap'_hred.
 Qed.
 
-Lemma subst_to_var : forall t t' i,
-  subst 0 t' t = Var i -> t = Var 0 /\ t' = Var i \/ t = Var (i + 1).
+Lemma subst_to_var : forall M N i,
+  subst 0 N M = Var i -> M = Var 0 /\ N = Var i \/ M = Var (i + 1).
 Proof.
-  intros. induction t.
+  intros. induction M.
     (* Var *)
     inversion H. case_eq (Compare_dec.nat_compare n 0).
       (* Eq *)
       intros nEQ0. rewrite nEQ0 in H1. rewrite Compare_dec.nat_compare_eq_iff in nEQ0.
       left. split.
         f_equal. assumption.
-        rewrite (lift_0_ident t' 0). reflexivity.
+        rewrite (lift_0_ident N 0). reflexivity.
       (* Lt *)
       intro nLT0. rewrite <- Compare_dec.nat_compare_lt in nLT0. contradict nLT0. omega.
       (* Gt *)
@@ -295,8 +295,8 @@ Proof.
     simpl in H. inversion H.
 Qed.
 
-Lemma subst_indist : forall i r r',
-  i > 0 -> subst 0 r (Var i) = subst 0 r' (Var i).
+Lemma subst_indist : forall i M N,
+  i > 0 -> subst 0 M (Var i) = subst 0 N (Var i).
 Proof.
   intros. simpl. rewrite Compare_dec.nat_compare_gt in H. rewrite H. reflexivity.
 Qed.
@@ -305,22 +305,22 @@ Qed.
    only due to the reflexive rule.
 *)
 
-Lemma not_hap'_lam_var : forall t i, ~ hap' (Lam t) i.
+Lemma not_hap'_lam_var : forall M i, ~ hap' (Lam M) i.
 Proof. unfold not. intros. dependent induction H. Qed.
 
-Lemma not_hap_lam : forall t t', t' <> Lam t -> ~ hap (Lam t) t'.
+Lemma not_hap_lam : forall M N, N <> Lam M -> ~ hap (Lam M) N.
 Proof.
   unfold not. intros.
   dependent induction H0.
-    assert (ID: Lam t = Lam t) by reflexivity. apply H in ID. assumption.
+    assert (ID: Lam M = Lam M) by reflexivity. apply H in ID. assumption.
     contradict H0. apply not_hap'_lam_var.
     apply IHhap2 in H. assumption. apply NNPP in IHhap1. assumption.
 Qed.
 
-Lemma lift_first_hap : forall t n i k,
-  hap t (Var i) -> i < k -> hap (lift n k t) (Var i).
+Lemma lift_first_hap : forall M n i k,
+  hap M (Var i) -> i < k -> hap (lift n k M) (Var i).
 Proof.
-  intros t n i k H iLTk.
+  intros M n i k H iLTk.
   assert (Var i = lift n k (Var i)). simpl. case_eq (Compare_dec.lt_dec i k).
     (* i < k *)
     intros. reflexivity.
@@ -329,41 +329,41 @@ Proof.
   rewrite H0. apply lift_hap. assumption.
 Qed.
 
-Lemma lift_st : forall t t',
-  st t t' -> forall n k, st (lift n k t) (lift n k t').
+Lemma lift_st : forall M N,
+  st M N -> forall n k, st (lift n k M) (lift n k N).
 Proof.
-  intros t t' H. dependent induction H.
+  intros M N H. dependent induction H.
     (* st_hap *)
-    intros n k. simpl. case_eq (Compare_dec.lt_dec i k).
+    intros n k. simpl. case_eq (Compare_dec.lt_dec x k).
       (* i < k *)
       intros. apply st_hap. apply lift_first_hap; assumption.
       (* ~ i < k *)
       intros. apply st_hap.
-      assert (FLD: Var (i + n) = lift n k (Var i)). simpl.
-        case_eq (Compare_dec.lt_dec i k). intros. contradiction. intros. reflexivity.
+      assert (FLD: Var (x + n) = lift n k (Var x)). simpl.
+        case_eq (Compare_dec.lt_dec x k). intros. contradiction. intros. reflexivity.
       rewrite FLD. apply lift_hap. assumption.
     (* st_hap_st_st *)
-    intros n k. apply st_hap_st_st with (t1 := lift n k t1) (t2 := lift n k t2).
+    intros n k. apply st_hap_st_st with (A := lift n k A) (B := lift n k B).
     rewrite <- lift_app. apply lift_hap. assumption.
     fold lift. apply IHst1.
     fold lift. apply IHst2.
     (* st_haplam_st *)
-    intros n k. apply st_haplam_st with (t1 := lift n (k+1) t1).
+    intros n k. apply st_haplam_st with (A := lift n (k+1) A).
     rewrite <- lift_lam. apply lift_hap. assumption.
     fold lift. apply IHst.
 Qed.
 
 (* (5) *)
-Lemma st_st__st_subst : forall t1 t2 t3 t4,
-  st t1 t2 -> st t3 t4 -> forall n, st (subst n t3 t1) (subst n t4 t2).
+Lemma st_st__st_subst : forall M N P Q,
+  st M N -> st P Q -> forall n, st (subst n P M) (subst n Q N).
 Proof.
-  intros t1 t2 t3 t4 HMN HPQ. induction HMN.
+  intros M N P Q HMN HPQ. induction HMN.
     (* hap with Var *)
     intro n.
-    assert (hap (subst n t3 t) (subst n t3 t)). apply hap_refl.
-    assert (hap (subst n t3 t) (subst n t3 (Var i))). apply hap__hap_subst. assumption.
-    apply hap_st__st with (t2 := subst n t3 (Var i)). assumption.
-    simpl. case_eq (Compare_dec.nat_compare i n).
+    assert (hap (subst n P L) (subst n P L)). apply hap_refl.
+    assert (hap (subst n P L) (subst n P (Var x))). apply hap__hap_subst. assumption.
+    apply hap_st__st with (M := subst n P (Var x)). assumption.
+    simpl. case_eq (Compare_dec.nat_compare x n).
       (* Eq *)
       intro. apply lift_st. assumption.
       (* Lt *)
@@ -372,13 +372,13 @@ Proof.
       intro. apply st_hap. apply hap_refl.
     (* hap with App *)
     intro n. rewrite subst_app.
-    apply st_hap_st_st with (t1 := subst n t3 t1) (t2 := subst n t3 t2).
+    apply st_hap_st_st with (A := subst n P A) (B := subst n P B).
     rewrite <- subst_app.
     apply hap__hap_subst.
     assumption. apply (IHHMN1 n). apply (IHHMN2 n).
     (* hap with Lam *)
     intro n.
-    apply st_haplam_st with (t1 := subst (n+1) t3 t1).
+    apply st_haplam_st with (A := subst (n+1) P A).
     rewrite <- subst_lam.
     apply hap__hap_subst.
     assumption.
@@ -391,9 +391,9 @@ Lemma st_app__st_subst : forall L M N,
   st L (App (Lam M) N) -> st L (subst 0 N M).
 Proof.
   intros. inversion H as [ | X P N' |]. inversion H4 as [ | | X' M' ].
-  apply hap_st__st with (t2 := subst 0 N' M').
-  apply hap_trans with (t2 := App (Lam M') N').
-  apply hap_trans with (t2 := App P N').
+  apply hap_st__st with (M := subst 0 N' M').
+  apply hap_trans with (B := App (Lam M') N').
+  apply hap_trans with (B := App P N').
     assumption.
     apply hap__app_hap. apply H7.
     apply hap_hred. apply hap'_hred.
@@ -413,35 +413,35 @@ Proof.
     (* 2.1 (2) *)
     intros L Hst.
     inversion Hst as [| X A' C' |].
-    apply st_hap_st_st with (t1 := A') (t2 := C').
+    apply st_hap_st_st with (A := A') (B := C').
       assumption.
       apply IHHbet. assumption.
       assumption.
     (* 2.1 (3) *)
     intros L Hst.
     inversion Hst as [| X A' C' |].
-    apply st_hap_st_st with (t1 := A') (t2 := C').
+    apply st_hap_st_st with (A := A') (B := C').
       assumption.
       apply (IHHbet A'); assumption.
       assumption.
     (* 2.1 (4) *)
     intros L Hst.
     inversion Hst as [| X A' C' |].
-    apply st_hap_st_st with (t1 := A') (t2 := C').
+    apply st_hap_st_st with (A := A') (B := C').
       assumption.
       assumption.
       apply (IHHbet C'); assumption.
     (* 2.1 (5) *)
     intros L Hst.
     inversion Hst as [| X A' C' |].
-    apply st_hap_st_st with (t1 := A') (t2 := C').
+    apply st_hap_st_st with (A := A') (B := C').
       assumption.
       assumption.
       apply (IHHbet C'); assumption.
     (* 2.1 (6) *)
     intros L Hst.
     inversion Hst as [| | X A'].
-    apply st_haplam_st with (t1 := A').
+    apply st_haplam_st with (A := A').
       assumption.
       apply (IHHbet A'). assumption.
 Qed.
@@ -464,7 +464,6 @@ Proof.
     apply (st_bred__st x x y). assumption. apply st_refl.
     (* refl *)
     apply st_refl.
-    SearchAbout clos_refl_trans.
     (* trans *)
     apply (Operators_Properties.clos_refl_trans_ind_left lterm bet y).
       assumption.
